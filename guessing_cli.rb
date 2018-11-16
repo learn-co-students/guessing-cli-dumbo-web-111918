ca78137fc1939da8cb3598abc1b4190e8482267e
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 21:22:00 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,9 @@
 # Code your solution here!
+def run_guessing_game
+  usr_input = ""
+  until usr_input = "exit"
+    comp_guess = rand(100)
+    usr_input = gets.chomp
+    usr_input.to_i == comp_guess ? puts "You guessed the correct number!":"The computer guessed #{comp_guess}."
+  end
+end
